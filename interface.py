a6fddbf1bc9187b7225be79e1fdc12e6da7aeb21
SpaghettiWriter <thao05trinh05@gmail.com>
2024-09-21 16:41:34 -0500

    Added and interface and changed filenames

---
+++ interface.py
@@ -0,0 +1,14 @@
+import choiceCalc
+import data
+import roundAuto
+import roundRules
+
+RoundRules = roundRules.RoundRules
+roundAuto = roundAuto
+Data = data.tableCl
+Round = choiceCalc.Round
+
+Round.GetChoiceInNum()
+Round.ChoiceAi()
+
+
